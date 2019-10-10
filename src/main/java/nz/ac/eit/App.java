33f4faeac90f677af1d236898e0c6943753a8e3e
PixelBlayd <backflipbomb12@gmail.com>
2019-10-10 13:13:33 +1300

    Added Random Function and a few initialization fields

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -1,13 +1,30 @@
 package nz.ac.eit;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    /** Random Number Generator found at : https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values**/
+    public void randomRoll(){
+        Random r = new Random();
+        int low = 1;
+        int high = 10;
+        int result = r.nextInt(high-low) + low;
+    }
+
+
+    /** Where The Main part starts**/
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int playerTotal = 0;
+        int computerTotal = 0;
+        System.out.println( "Hello World!");
+
+
+
     }
 }
